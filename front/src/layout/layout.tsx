2a0c6858d713ab62c16f3db841bc324ac0fd4c2a
Req <snogedirl@gmail.com>
2025-05-03 17:34:06 +0200

    header & mobile navbar; install react query react router tailwindcss

---
+++ front/src/layout/layout.tsx
@@ -0,0 +1,14 @@
+import { Outlet } from "react-router";
+import Header from "../components/header";
+
+function Layout() {
+  return (
+    <div className="grid h-dvh grid-rows-[auto_1fr] overflow-hidden">
+      <Header />
+      <main className="grow">
+        <Outlet />
+      </main>
+    </div>
+  );
+}
+export default Layout;
---
+++ front/src/pages/home-page.tsx
@@ -0,0 +1,9 @@
+function HomePage() {
+  return (
+    <div className="bg-lighest-beige flex h-full items-center justify-center">
+      <h1>Welcome to the Home Page</h1>
+    </div>
+  );
+}
+
+export default HomePage;
