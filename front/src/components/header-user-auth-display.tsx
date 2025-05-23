6c9fe5623ad15993fde0ec24fad54908bcc866a0
Req <snogedirl@gmail.com>
2025-05-23 11:16:39 +0200

    add logout functionality; fix nickname overflow

--- front/src/components/header-user-auth-display.tsx
+++ front/src/components/header-user-auth-display.tsx
@@ -63,8 +63,16 @@ function LoggedInUserProfile({
     };
   }, []);
 
-  const handleLogout = () => {
-    console.log("Logging out...");
+  const handleLogout = async () => {
+    try {
+      await fetch("http://localhost:8080/auth/logout", {
+        method: "POST",
+        credentials: "include",
+      });
+      window.location.href = "/login";
+    } catch (error) {
+      console.error("Error logging out:", error);
+    }
   };
 
   return (
@@ -78,7 +86,7 @@ function LoggedInUserProfile({
           aria-label={`Logged in as ${user.name}. Click to open user menu.`}
         >
           <UserAvatarDisplay user={user} isLoggedIn={true} />
-          <p className="line-clamp-1 max-w-20 overflow-y-auto">{user.name}</p>
+          <p className="line-clamp-1 max-w-20">{user.name}</p>
         </button>
         {isOpen && (
           <div
