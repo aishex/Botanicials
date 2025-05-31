a9278afc65208ea1c89024c4b9f3948a36ee1566
Zuza <hynekzuzanna@gmail.com>
2025-05-31 11:26:08 +0200

    add example sql data

---
+++ backend/src/main/resources/data.sql
@@ -0,0 +1,18 @@
+-- test user
+INSERT INTO users (id, email, name, google_id, image_url)
+VALUES (50, 'test@example.com', 'Test User', '12345', 'https://ipicasso.pl/image/cache/data/goods/00000004867-800x800.jpg');
+
+-- post
+INSERT INTO forum_post (id, user_id, title, content, image_url, created_at)
+VALUES (
+  1,
+  50,
+  'Jak podlewać monsterę?',
+  'Mam problem z żółknięciem liści. Co robić?',
+  'https://v.wpimg.pl/ZTMzMjc0dTUsGDtZTEt4IG9AbwMKEnZ2OFh3SEwGamR9AnlaUwB1MiQWKBgGQDV6Kgg4GgJHKno9FmILE1l1InxVKQMQQDY1NFUoBwFVPnt8GHVaBwlpZ2AfeAsFHW5nek9gU1EFY3l8SStSVlVpZ39NeQ9BTQ',
+  NOW()
+);
+
+--comment
+INSERT INTO forum_comments (id, forum_post_id, user_id, content, created_at)
+VALUES (1, 1, 50, 'Spróbuj podlewać raz na 10 dni i nie przestawiaj jej.', NOW());
